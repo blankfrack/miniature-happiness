a2ea432b7af113008e005da070b0ae44b97deed6
blankfrack <githubbbe97531>
2014-11-15 15:54:14 -0500

    Creation

---
+++ Test/Test/src/com/blankfrack/test/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.blankfrack.test;
+
+/**
+ * @author bux
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World !");
+
+	}
+
+}
